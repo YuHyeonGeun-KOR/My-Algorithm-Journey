8513e4d75ce7d8d91688154ab8e1221c38de43c5
YuHyeonGeun-KOR <63186208+YuHyeonGeun-KOR@users.noreply.github.com>
2020-12-01 16:31:54 +0900

    Complete(pythony)

--- This is cote/Implementation/up_down_left_right.py
+++ This is cote/Implementation/up_down_left_right.py
@@ -2,16 +2,18 @@ X = 1
 Y = 1
 
 N = int(input())
-direct_list = list(input().split())
+direct_lists = list(input().split())
 
-for i in range(len(direct_list)):
-    if direct_list[i] == "R" and Y < N:
+for direct_list in direct_lists:
+    if direct_list == "R" and Y < N:
         Y += 1
-    elif direct_list[i] == "L"and Y > 1:
+    elif direct_list == "L"and Y > 1:
         Y -= 1     
-    elif direct_list[i] == "D"and X < N:
+    elif direct_list == "D"and X < N:
         X += 1     
-    elif direct_list[i] == "U"and X > 1:
+    elif direct_list == "U"and X > 1:
         X -= 1     
- 
+    else :
+        continue
+
 print(X,Y)
