a34d584c1ba9f1d389841d846bc70356f51204aa
YuHyeonGeun-KOR <63186208+YuHyeonGeun-KOR@users.noreply.github.com>
2020-12-07 22:59:46 +0900

    complete

--- This is cote/Implementation/gamedevelop.py
+++ This is cote/Implementation/gamedevelop.py
@@ -8,40 +8,42 @@ check = [[0]*M for j in range(N)]
 result = 0
 count = 0
 
-Left = [(0,-1),(1,0),(0,1),(-1,0)] 
-Back = [(-1,0),(0,-1),(1,0),(0,1)]
 
+Left = [(0,-1),(0,-1),(0,1),(1,0)] 
+Back = [(1,0),(0,-1),(-1,0),(0,1)]
+check =Filed
 check[current_Y][current_X] = 1
-
+result += 1
+print(Filed)
 print(check)
-print(current_Y+Left[D][0])
-print(current_X + Left[D][1])
 
 while(1) :
-    if Filed[current_Y+Left[D][0]][current_X + Left[D][1]] == 0 and check[current_Y+Left[D][0]][current_X + Left[D][1]] == 0:
+    print ("Direction :" , D)
+    if Filed[current_X + Left[D][0]][current_Y + Left[D][1]] == 0 and check[current_X+Left[D][0]][current_Y + Left[D][1]] == 0:
+        current_X = current_X + Left[D][0]
+        current_Y = current_Y + Left[D][1]
         D -= 1
+        
         if D < 0 :
             D = 3
-            count = 0 
-    
-        current_Y = current_Y+Left[D][0]
-        current_X = current_X+Left[D][1]
+        count = 0 
 
         result += 1
 
-    elif Filed[current_Y+Left[D][0]][current_X + Left[D][1]] == 1 or check[current_Y+Left[D][0]][current_X + Left[D][1]] == 1 :
+    elif Filed[current_X+Left[D][0]][current_Y + Left[D][1]] == 1 or check[current_X+Left[D][0]][current_Y + Left[D][1]] == 1 :
         D -= 1
         if D < 0 :
             D = 3
         count += 1
 
-
-    if count == 4 :
-        count = 0
-        current_Y = current_Y+Back[D][0]
-        current_X = current_X+Back[D][1]
-        if Filed[current_Y][current_X] == 1 :
+    if count == 3 :
+        current_X = current_X+Back[D][0]
+        current_Y = current_Y+Back[D][1]
+        if Filed[current_X][current_Y] == 1 :
             break
-
+        else :
+            count = 0    
+    
+    print("current_X ,Y : " , current_X , current_Y )    
 
 print(result)        
