4fc42e92a3e1e4a58851d14b057a61f5c263e7eb
YuHyeonGeun-KOR <63186208+YuHyeonGeun-KOR@users.noreply.github.com>
2020-12-19 21:53:45 +0900

    compolete

---
+++ This is cote/Programmers/doll game.py
@@ -0,0 +1,17 @@
+def solution(board, moves):
+    answer = 0
+    moved=[0]  
+    for i in moves:
+        i-=1
+        for j in range(len(board)):
+            if board[j][i] !=0:   
+                moved.append(board[j][i]) 
+                board[j][i]=0  
+                if moved[-1] == moved[-2]:                
+                    moved.pop(-1)    
+                    moved.pop(-1)
+                    answer+=2 
+                break  
+    
+                
+    return answer
