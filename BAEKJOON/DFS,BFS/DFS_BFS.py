145cc8e1094f1c38fb6b93d8b1da6f4aa3f82ac3
username <YuHyeonGuen.dev@gmail.com>
2020-12-31 16:01:22 +0900

    Complete

---
+++ BAEKJOON/DFS,BFS/DFS_BFS.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+
+n,m,start = map(int, input().split())
+
+
+def dfs(graph,v,visited):
+    visited[v] = True
+    print(v,end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+def bfs(graph,start,visited):
+    queue = deque([start])
+    visited[start]= True
+    while queue:
+        v = queue.popleft()
+        print(v,end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]= True
+
+graph = [[] for i in range(n+1)]
+
+visited = [False]*(n+1)
+visited1 = [False]*(n+1)
+for i in range(0,m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1,n+1):
+    graph[i].sort()
+
+dfs(graph,start,visited) 
+print('')
+bfs(graph,start,visited1)   
