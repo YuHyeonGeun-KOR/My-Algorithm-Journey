4f1c14b3920fe6437d3881bdefe050f0a0d770fe
username <YuHyeonGuen.dev@gmail.com>
2021-01-24 22:55:20 +0900

    Complete

---
+++ This is cote/Programmers/Hash/Marathon.py
@@ -0,0 +1,24 @@
+def solution(participant, completion):
+    answer = ''
+    
+    count = {}
+    for i in participant:
+        try: count[i] += 1
+        except: count[i] = 1
+
+   
+    for i in completion:
+        count[i] -= 1
+
+
+    for part, compl in count.items():
+        if compl != 0:
+            answer = part
+
+    return answer
+
+
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+
+print(solution(participant, completion))
