d243ca1049f2a9197261cad99f81a2afbb2f5f08
username <YuHyeonGuen.dev@gmail.com>
2021-01-24 23:35:34 +0900

    Complete

---
+++ This is cote/Programmers/Hash/PhoneBook.py
@@ -0,0 +1,16 @@
+def solution(phone_book):
+    answer = True
+    for i in phone_book:
+        origin_list = list(i)
+        for j in phone_book:
+            if i != j :
+
+                check_list = list(j)
+                if check_list[0:len(origin_list)] == origin_list:
+                    answer = False 
+                    return answer
+    return answer
+
+phone_book = ["123","456", "789"]
+
+print(solution(phone_book))
