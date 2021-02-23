5e91fa1ca78a400f5030474f2fca6bd817c1e0f8
username <YuHyeonGuen.dev@gmail.com>
2021-02-23 10:51:45 +0900

    Complete

--- BAEKJOON/DFS,BFS(done)/14502(lab).py
+++ BAEKJOON/DFS,BFS(done)/14502(lab).py
@@ -1,46 +1,63 @@
-s = []
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-max_result = 0
-def bfs():
-    global max_result
-    copy = [[0] * m for i in range(n)]
-    for i in range(n):
-        for j in range(m):
-            copy[i][j] = s[i][j]
-    result = 0
-    arr = []
-    for i in range(n):
-        for j in range(m):
-            if copy[i][j] == 2:
-                arr.append([i, j])
-    while arr:
-        a, b = arr[0][0], arr[0][1]
-        del arr[0]
-        for i in range(4):
-            ax = a + dx[i]
-            ay = b + dy[i]
-            if 0 <= ax and 0 <= ay and ax < n and ay < m:
-                if copy[ax][ay] == 0:
-                    copy[ax][ay] = 2
-                    arr.append([ax, ay])
-    for i in copy:
-        for j in i:
-            if j == 0:
-                result += 1
-    max_result = max(max_result, result)
-def wall(cnt):
-    if cnt == 3:
-        bfs()
-        return
-    for i in range(n):
-        for j in range(m):
-            if s[i][j] == 0:
-                s[i][j] = 1
-                wall(cnt + 1)
-                s[i][j] = 0
-n, m = map(int, input().split())
+import sys
+input = sys.stdin.readline
+from itertools import combinations
+from collections import deque
+import copy
+
+n,m = map(int, input().split())
+
+lab = []
+index = []
+virus = []
+safe_zone = []
+index_num = 0
+
 for i in range(n):
-    s.append(list(map(int, input().split())))
-wall(0)
-print(max_result)
+    lab.append(list(map(int,input().split())))
+
+for i in range(n):
+    for j in range(m):
+        index.append((i,j))
+        if lab[i][j] == 2:
+            virus.append((i,j))
+
+wall = list(combinations(index,3))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(wall_index):
+    queue = deque()
+    for i in virus:
+        queue.append((i[0],i[1]))
+    
+    lab_copy = copy.deepcopy(lab)
+    if lab_copy[wall_index[0][0]][wall_index[0][1]] ==0 and lab_copy[wall_index[1][0]][wall_index[1][1]] == 0 and lab_copy[wall_index[2][0]][wall_index[2][1]]==0:
+        lab_copy[wall_index[0][0]][wall_index[0][1]] = 1
+        lab_copy[wall_index[1][0]][wall_index[1][1]] = 1
+        lab_copy[wall_index[2][0]][wall_index[2][1]] = 1
+    else:
+         return
+    safe_num = 0
+
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<= nx < n and 0 <= ny < m and lab_copy[nx][ny] == 0:
+                lab_copy[nx][ny] = 2
+                queue.append((nx,ny))
+        
+    for i in range(n):
+        for j in range(m):
+            if lab_copy[i][j] == 0:
+                safe_num+=1
+    safe_zone.append(safe_num)
+    
+    return
+for i in wall:
+    bfs(i)
+
+print(max(safe_zone))
