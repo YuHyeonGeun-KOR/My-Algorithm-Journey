881891ea51e91986dde30e4d1a1e2ed511c612dd
username <YuHyeonGuen.dev@gmail.com>
2021-03-20 16:43:30 +0900

    try again

---
+++ 스코페/test2.py
@@ -0,0 +1,34 @@
+n = int(input())
+
+transport = list(input())
+checker = [0]*(n)
+
+for i in range(0,n):
+    if i+1 == n-1:
+        if transport[i] =='0'or transport[i-1] == '0':
+            if checker[i-2] == 0:
+                checker[i+1] = 1    
+            else:
+                checker[i+1] = checker[i-2]
+            break
+        else:
+            break
+    elif transport[i] == '0':
+        checker[i+1] = checker[i]
+        checker[i+2] = checker[i]
+        continue
+    else:
+        if (transport[i+1] == '0'):
+            if checker[i+1] ==0: 
+                checker[i+1] = checker[i]
+        elif transport[i+2] == '0':
+            if checker[i+2] ==0:
+                checker[i+2] = checker[i]
+        else:
+            checker[i+1] = checker[i+1]+1
+            checker[i+2] = checker[i]+2
+        
+
+print(checker[-1])
+    
+
