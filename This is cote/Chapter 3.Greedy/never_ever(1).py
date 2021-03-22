aaea4ae92f41e5b02617d8ebc4294c275d53d304
username <YuHyeonGuen.dev@gmail.com>
2021-03-22 14:04:21 +0900

    Complete

---
+++ This is cote/Chapter 3.Greedy/never_ever(1).py
@@ -0,0 +1,10 @@
+n,m = map(int, input().split())
+
+
+ball = list(map(int,input().split()))
+result = 0
+for i in range(len(ball)-1):
+    for j in range(i+1,len(ball)):
+        if ball[i] != ball[j]:
+            result +=1
+print(result)
