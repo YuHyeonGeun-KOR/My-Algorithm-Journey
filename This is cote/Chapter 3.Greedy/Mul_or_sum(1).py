111e01c21890d3b34a33608574c5ed1d985013a7
username <YuHyeonGuen.dev@gmail.com>
2021-03-25 23:55:26 +0900

    Complete

--- This is cote/Chapter 3.Greedy/Mul_or_sum(1).py
+++ This is cote/Chapter 3.Greedy/Mul_or_sum(1).py
@@ -6,7 +6,7 @@ print(number_quque)
 num_1 = int(number_quque.popleft())
 num_2 = int(number_quque.popleft())
 num_sum = 0
-if num_1 == 0 or num_2 == 0:
+if num_1 == 0 or num_2 == 0 or num_1 == 1 or num_2 == 1:
     num_sum = num_1 + num_2
 else :
     num_sum = num_1 * num_2
@@ -16,6 +16,8 @@ for i in range(len(number_quque)):
     num_1 = int(number_quque.popleft())
     if num_1 == 0 :
         continue
+    elif num_1 ==1:
+        num_sum = num_sum + 1
     else :
         num_sum = num_sum * num_1
 
