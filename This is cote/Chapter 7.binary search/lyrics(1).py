52603c3e58b56e47e8a8bf374946db3d53e5dfaf
username <YuHyeonGuen.dev@gmail.com>
2021-04-28 10:10:23 +0900

    Update clear

--- This is cote/Chapter 7.binary search/lyrics(1).py
+++ This is cote/Chapter 7.binary search/lyrics(1).py
@@ -19,14 +19,19 @@ def solution (words , queries):
             left_checker = i[::-1].replace('?' , 'a')
             right_checker = i[::-1].replace('?','z')
             left_index = bisect_left(word_reverse[len(i)],left_checker)
-            right_index = bisect_left(word_reverse[len(i)],right_checker)
+            right_index = bisect_right(word_reverse[len(i)],right_checker)
             count = right_index-left_index
         else:
             left_checker = i.replace('?' , 'a')
             right_checker = i.replace('?','z')
             left_index = bisect_left(word_length[len(i)],left_checker)
-            right_index = bisect_left(word_length[len(i)],right_checker)
+            right_index = bisect_right(word_length[len(i)],right_checker)
             count = right_index-left_index        
         answer.append(count)
 
     return answer
+
+words = ["frodo", "frozz", "frozz", "frozen", "frame", "kakao"]
+queries = ["fro??"]
+
+print(solution(words, queries))
