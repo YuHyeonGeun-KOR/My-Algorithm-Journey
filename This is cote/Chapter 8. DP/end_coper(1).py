44a56015d96d5d7fc2edbe21aa358a6f4e3c3454
username <YuHyeonGuen.dev@gmail.com>
2021-05-03 16:14:35 +0900

    Complete

---
+++ This is cote/Chapter 8. DP/end_coper(1).py
@@ -0,0 +1,23 @@
+n = int(input())
+
+work = {}
+for i in range(n):
+    work[i+1] = list(map(int,input().split()))
+
+start_to_end_income = [0] * (10000)
+time = 0
+income =0
+result = []
+for i in range(1,n+1):
+    time = work[i][0]
+    income = work[i][1]
+    if start_to_end_income[i] > start_to_end_income[i+1]:
+        start_to_end_income[i+1] = start_to_end_income[i]
+
+    if start_to_end_income[time + i] < start_to_end_income[i] + income:
+         start_to_end_income[time + i] = start_to_end_income[i] + income
+    
+
+
+        
+print(start_to_end_income[n+1])
