ed9218b36282a3d54311757832a61ac82840bea6
username <YuHyeonGuen.dev@gmail.com>
2021-05-04 16:21:47 +0900

    Complete

---
+++ This is cote/Chapter 8. DP/uglynumber(1).py
@@ -0,0 +1,20 @@
+n = int(input())
+
+ugly_num = []
+
+for i in range(1,1001):
+    num_list = []
+    checker = i
+    while True:
+        if checker % 2 == 0:
+            checker = checker//2    
+        elif checker % 3 == 0:
+            checker = checker//3
+        elif checker % 5 == 0:
+            checker = checker//3
+        elif checker == 1:
+            ugly_num.append(i)
+            break
+        elif checker !=1:
+            break
+print(ugly_num)
