795bb9dfec46d8f735892658f9fffcf80dacbdd5
username <YuHyeonGuen.dev@gmail.com>
2021-05-04 13:48:06 +0900

    Complete

---
+++ This is cote/Chapter 8. DP/Soldier(1).py
@@ -0,0 +1,26 @@
+n = int(input())
+
+
+num_list = list(map(int,input().split()))
+num_list.reverse()
+new_list = [0] *(2*n)
+new_list[0] = num_list[0]
+
+
+def bst(start, end , array,target):
+    while start< end:
+        mid = (start+end) //2
+        if array[mid] > target:
+            start = mid+ 1
+        elif array[mid] < target:
+
+for i in range(n):
+    for j in range(i):
+        if num_list[j] > new_list[i] :
+            new_list[i+1] = num_list[j]
+        else:
+            
+
+        
+print(count)  
+    
