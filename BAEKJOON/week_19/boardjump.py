0c06a8c0296378fdab6b981600a8e66741b31ad6
username <YuHyeonGuen.dev@gmail.com>
2022-02-09 18:17:08 +0900

    Complete

---
+++ BAEKJOON/week_19/boardjump.py
@@ -0,0 +1,30 @@
+from http.client import BAD_REQUEST
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n= int(input())
+
+board = []
+
+queue = deque([[0,0]])
+
+result = 0
+visited = [[0] * n for _ in range(n)]
+
+for i in range(n):
+    board.append(list(map(int, input().split())))
+
+  
+for i in range(n):
+    for j in range(n):
+        if board[i][j] != 0 : 
+            jump = board[i][j]
+            if 0 <= i+jump < n :
+                visited[i+jump][j] = visited[i][j] +1
+            if 0 <= j + jump < n:
+                visited[i][j+jump] = visited[i][j] +1
+                
+print(visited)
+     
+
